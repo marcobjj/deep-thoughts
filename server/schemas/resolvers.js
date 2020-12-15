5ac61c731ab51cd01172664ce2d9b18a067ff8d0
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-12-15 12:58:41 -0800

    adds react frontend and if-env

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -94,6 +94,7 @@ const resolvers = {
           },
           login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
+            
           
             if (!user) {
               throw new AuthenticationError('Incorrect credentials');
